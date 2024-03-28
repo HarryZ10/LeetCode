ee4154ce9a8e032c7f38b3c5f2f0d858c1b108db
Harry <32529321+HarryZ10@users.noreply.github.com>
2024-03-28 08:19:54 -0700

    :ballot_box_with_check: same-tree [Runtime: 33 ms; Memory: 16.6 MB]

--- depth-first-search/same-tree.py
+++ depth-first-search/same-tree.py
@@ -6,9 +6,10 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q: return True
-        if not p or not q: return False
-        if p and q and p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        return False
+        return hash(str(p)) == hash(str((q)))
+        # if not p and not q: return True
+        # if not p or not q: return False
+        # if p and q and p.val == q.val:
+        #     return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # return False
             
